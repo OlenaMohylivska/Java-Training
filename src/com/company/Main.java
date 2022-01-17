15e2567c3dbc6313811242fc06290204b60773f1
EXADEL\omohylivska <mogilevskaya1994@gmail.com>
2022-01-17 20:16:05 +0200

    init

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int num1 = 11;
+        int num2 = 4;
+        int result = num1 + num2;
+
+        System.out.println("Result: " + result);
+    }
+}
